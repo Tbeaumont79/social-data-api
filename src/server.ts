df8e35bdba08948d32105b5522338a475f7f8049
tbeaumont79 <tbeaumont79@icloud.com>
2024-12-12 20:04:33 +0100

    Feat: the server is now running

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
