f4610026672e5100dad1ab993efb1e5dc990e6e2
tbeaumont79 <tbeaumont79@icloud.com>
2024-12-16 13:06:37 +0100

    Fix: [Bugfix] the app.use api was missing in the server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
-
+import api from "./routes/api";
 dotenv.config();
 
 const app = express();
@@ -8,7 +8,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
+app.use("/api", api);
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
