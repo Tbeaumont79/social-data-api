c3d488bbb9c3066ff1b5d5ab6ff551943aa8711e
tbeaumont79 <tbeaumont79@icloud.com>
2024-12-28 08:25:32 +0100

    Feat: Working on parsing github changelogs

--- src/controllers/githubController.ts
+++ src/controllers/githubController.ts
@@ -4,7 +4,6 @@ import { fetchVueJsChangelogs } from "../services/githubService";
 export const getChangelogHandler = async (req: Request, res: Response) => {
   try {
     const changelogs = await fetchVueJsChangelogs();
-    console.log(changelogs);
     res.status(200).json({ success: true, data: changelogs });
   } catch (error) {
     console.error("Error fetching releases:", error);
--- src/services/githubService.ts
+++ src/services/githubService.ts
@@ -1,5 +1,27 @@
 import axios from "axios";
 
+const parsing = (content: string) => {
+  const lines = content.split("\n##");
+  let recentSection = lines.splice(1, 3);
+  recentSection
+    .map((section) => {
+      const [versionLine, ...changes] = section.split("\n");
+      const versionMatch = versionLine.match(
+        /\[v([\d.]+)] - (\d{4}-\d{2}-\d{2})/
+      );
+      if (!versionMatch) {
+        return null;
+      }
+      return {
+        version: versionMatch[1],
+        date: versionMatch[2],
+        changes: changes.filter((line) => line.trim().startsWith("-")),
+      };
+    })
+    .filter(Boolean);
+  console.log(recentSection);
+  return recentSection;
+};
 export const fetchVueJsChangelogs = async () => {
   try {
     const response = await axios.get(
@@ -7,6 +29,8 @@ export const fetchVueJsChangelogs = async () => {
     );
     const contentBase64 = response.data.content;
     const content = Buffer.from(contentBase64, "base64").toString("utf-8");
+
+    parsing(content);
     return content;
   } catch (error) {
     console.error("Error fetching cahngelog:", error);
